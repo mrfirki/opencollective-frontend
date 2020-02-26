62cbc3a530ec32eacefcce1e75caf37b99020067
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2020-02-26 13:09:04 +0100

    feat(ExpensePage): Implement new page

--- components/expenses/PayoutMethodData.js
+++ components/expenses/PayoutMethodData.js
@@ -1,6 +1,29 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { get } from 'lodash';
 import { PayoutMethodType } from '../../lib/constants/payout-method';
+import { FormattedMessage } from 'react-intl';
+import { Span } from '../Text';
+
+/**
+ * @returns boolean: True if the payout method has displayable data
+ */
+export const payoutMethodHasData = payoutMethod => {
+  switch (payoutMethod.type) {
+    case PayoutMethodType.PAYPAL:
+      return Boolean(get(payoutMethod, 'data.email'));
+    case PayoutMethodType.OTHER:
+      return Boolean(get(payoutMethod, 'data.content'));
+    default:
+      return false;
+  }
+};
+
+const PrivateFallback = () => (
+  <Span color="black.500" fontStyle="italic">
+    <FormattedMessage id="Private" defaultMessage="Private" />
+  </Span>
+);
 
 /**
  * Shows the data of the given payout method
@@ -8,9 +31,9 @@ import { PayoutMethodType } from '../../lib/constants/payout-method';
 const PayoutMethodData = ({ payoutMethod }) => {
   switch (payoutMethod.type) {
     case PayoutMethodType.PAYPAL:
-      return get(payoutMethod, 'data.email');
+      return get(payoutMethod, 'data.email') || <PrivateFallback />;
     case PayoutMethodType.OTHER:
-      return get(payoutMethod, 'data.content');
+      return get(payoutMethod, 'data.content') || <PrivateFallback />;
     default:
       return null;
   }
